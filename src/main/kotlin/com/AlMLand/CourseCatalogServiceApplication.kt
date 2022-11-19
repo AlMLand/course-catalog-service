16c53fdbe77cb5ada4639f73a6a272a79b24b858
Alex Morland <morlandalex@googlemail.com>
2022-11-19 23:03:06 +0100

    add : greeting controller and service, logging dependency, 'prod' profile

--- src/main/kotlin/com/AlMLand/CourseCatalogServiceApplication.kt
+++ src/main/kotlin/com/AlMLand/CourseCatalogServiceApplication.kt
@@ -1,8 +1,8 @@
 package com.AlMLand
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-
 @SpringBootApplication
 class CourseCatalogServiceApplication
 
---
+++ src/main/kotlin/com/AlMLand/service/GreetingService.kt
@@ -0,0 +1,15 @@
+package com.AlMLand.service
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.PropertySource
+import org.springframework.stereotype.Service
+
+@PropertySource("classpath:application.yaml")
+@Service
+class GreetingService {
+    // pri ispolzovanii kotlin nuzhno ekranirovat znak $
+    @Value("\${message}")
+    lateinit var message: String
+
+    fun retrieveGreeting(name: String) = "$message, $name"
+}
