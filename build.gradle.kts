24f2ffdea0d6b0826b90b81eb784003d51129e62
Alex Morland <morlandalex@googlemail.com>
2022-12-04 23:43:12 +0100

    include liquibase to project
    - add init sql file
    - disabled hibernate.ddl-auto

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,9 +30,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     // logging
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // liquibase
+    implementation("org.liquibase:liquibase-core:4.17.2")
+
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
