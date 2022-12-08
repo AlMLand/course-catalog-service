08fcc076756a461f00500da6529491b0fc7113c0
Alex Morland <morlandalex@googlemail.com>
2022-12-08 21:28:47 +0100

    refactoring for profile demo

--- src/main/java/com/AlMLand/service/InstructorServiceJava.java
+++ src/main/java/com/AlMLand/service/InstructorServiceJava.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+//@Profile("default")
 public class InstructorServiceJava {
     private InstructorRepository repository;
 
