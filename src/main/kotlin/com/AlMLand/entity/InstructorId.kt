91e708b4d44e97a0cdeaf67c0a0bd8eeffe1b720
Alex Morland <Alex.Morland@ottogroup.com>
2023-01-27 10:52:30 +0100

    draft : not works

--- src/main/kotlin/com/AlMLand/entity/InstructorId.kt
+++ src/main/kotlin/com/AlMLand/entity/InstructorId.kt
@@ -1,13 +1,25 @@
 package com.AlMLand.entity
 
+import org.hibernate.annotations.ColumnTransformer
 import java.io.Serializable
 import javax.persistence.Column
 import javax.persistence.Embeddable
 
+private const val mySecretKey = "mySecretKey"
+
 @Embeddable
 data class InstructorId(
+    @ColumnTransformer(
+        read = "pgp_sym_decrypt(first_name, '$mySecretKey')",
+        write = "pgp_sym_encrypt(?, '$mySecretKey')"
+    )
     @field:Column(name = "first_name", nullable = false, updatable = true)
     val firstName: String,
+    @ColumnTransformer(
+        read = "pgp_sym_decrypt(last_name, '$mySecretKey')",
+        write = "pgp_sym_encrypt(?, '$mySecretKey')"
+    )
     @field:Column(name = "last_name", nullable = false, updatable = false)
     val lastName: String
 ) : Serializable
+// CREATE EXTENSION pgcrypto;
